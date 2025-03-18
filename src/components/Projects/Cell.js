31baef424e36223796e45840281183a2dde46e11
Anhar Risnumawan <anhrisn@gmail.com>
2025-03-18 22:19:02 +0900

    Replace anchor tags with Link components for internal navigation in Cell component

--- src/components/Projects/Cell.js
+++ src/components/Projects/Cell.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import dayjs from 'dayjs';
+import { Link } from 'react-router-dom';
 
 const Cell = ({ data }) => (
   <div className="cell-container">
     <article className="mini-post">
       <header>
         <h3>
-          <a href={data.link}>{data.title}</a>
+          <Link to={data.link}>{data.title}</Link>
         </h3>
         <time className="published">
           {dayjs(data.date).format('MMMM, YYYY')}
         </time>
       </header>
-      <a href={data.link} className="image">
+      <Link to={data.link} className="image">
         <img src={`${process.env.PUBLIC_URL}${data.image}`} alt={data.title} />
-      </a>
+      </Link>
       <div className="description">
         <p>{data.desc}</p>
       </div>
